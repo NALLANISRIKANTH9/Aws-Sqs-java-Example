febf7b02423b9485e798be145970716187116f46
Nallani <nallanisrikanth99@gmail.com>
2019-03-02 13:53:18 -0500

    Happy Messaging with SQS-JAVA

---
+++ src/main/java/com/sqs/awssqs/AwssqsApplication.java
@@ -0,0 +1,12 @@
+package com.sqs.awssqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwssqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwssqsApplication.class, args);
+	}
+}
